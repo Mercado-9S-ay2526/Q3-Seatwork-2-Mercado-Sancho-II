1ef0622329fa0be2d748d944cfe1373bc4f8007d
Mercado-9S-ay2526 <mercadosancholl@gmail.com>
2026-03-03 07:57:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 function seeDiscount(){
   let budget = document.getElementById("budget").value;
   let discountedvalue = budget * .2;
-  
-  document.getElementById(discount).innerHTML  ='
-  <div class="container" id="discount"> <div class="container p-5 mt-5 bg-dark text-light text-center rounded"> <p>Super Awesome Brands:<p> <button type="button" class="btn btn-outline-warning btn-sm">ACER</button><button type="button" class="btn btn-outline-warning btn-sm">ASUS</button><button type="button" class="btn btn-outline-warning btn-sm">Razer</button><button type="button" class="btn btn-outline-warning btn-sm">Lenovo</button><button type="button" class="btn btn-success" onclick="checkDiscount()">Check Discount</button><p>Eligible discount: <span></span></p></div> 
-    ';'
+
+document.getElementById("discountResult").innerHTML  = `
+ <div class="container" id="discount">
+        <div class="container p-5 mt-5 bg-dark text-light text-center rounded"> <p>Super Awesome Brands:<p>
+        <button type="button" class="btn btn-outline-warning btn-sm">ACER</button>
+        <button type="button" class="btn btn-outline-warning btn-sm">ASUS</button>
+        <button type="button" class="btn btn-outline-warning btn-sm">Razer</button>
+        <button type="button" class="btn btn-outline-warning btn-sm">Lenovo</button>
+        <button type="button" class="btn btn-success" onclick="seeDiscount()">See discount</button>
+        <p>Eligible Discount<p><span id=discountedvalue></span><p>
+    </div>
+
+`;
+document.getElementById("discountedvalue").innerHTML = discountedvalue;
 }
